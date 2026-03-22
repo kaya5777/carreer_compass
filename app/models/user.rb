b2aac766f03d38074c678b521fb04fd41f826cff
Manabu Kayamoto <manabukayamoto@ManabunoMacBook-Air-2.local>
2026-03-22 19:13:21 +0900

    remove password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
+         :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_omniauth(auth)
